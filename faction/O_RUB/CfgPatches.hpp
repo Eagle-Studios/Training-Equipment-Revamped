8c68c872e17c11921b6be4f07bfb42e063a67666
Cal Eagle <49170815+Eagle-Studios@users.noreply.github.com>
2023-01-10 16:42:35 +0000

    O_Rub (Faction, 3CB CW USSR)
    Rubber training faction using 3CB Cold War USSR

---
+++ faction/O_RUB/CfgPatches.hpp
@@ -0,0 +1,34 @@
+class CfgPatches {
+    class O_RUB {
+        units[] = {
+            "O_RUB_Rifleman_01",
+            "O_RUB_Marksman_01",
+            "O_RUB_Squad_Leader_01",
+            "O_RUB_Machine_Gunner_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "UK3CB_Factions_CW_SOV_O_EARLY",
+            "A3_Characters_F",
+            "A3_Sounds_F",
+            "A3_Sounds_F_Exp",
+            "ace_gforces",
+            "ace_goggles",
+            "ace_interaction",
+            "ace_movement",
+            "ace_medical_feedback",
+            "A3_Data_F",
+            "A3_Data_F_Curator",
+            "A3_Air_F_Heli",
+            "ace_thermals",
+            "ace_refuel",
+            "cba_xeh",
+            "training_core",
+            "training_mag_compat_762_AK",
+            "training_mag_compat_762x54"
+        };
+        author = "Maj. Eagle";
+        authors[] = {"Maj. Eagle"};
+    };
+};
